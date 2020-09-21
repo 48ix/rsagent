2bc49b3567c135c26c7a00b30c0c8502e9e788d1
checktheroads <matt@48ix.net>
2020-09-20 22:27:39 -0700

    ensure frr-reload is executable

--- rsagent/config.py
+++ rsagent/config.py
@@ -1,7 +1,11 @@
 """Read config variables from environment."""
 
+# Standard Library
+import os
+from pathlib import Path
+
 # Third Party
-from pydantic import FilePath, SecretBytes, BaseSettings, IPvAnyAddress
+from pydantic import FilePath, SecretBytes, BaseSettings, IPvAnyAddress, validator
 
 
 class Params(BaseSettings):
@@ -11,6 +15,18 @@ class Params(BaseSettings):
     listen_port: int = 4848
     frr_reload: FilePath = "/usr/lib/frr/frr-reload.py"
     key: SecretBytes
+
+    @validator("frr_reload")
+    def validate_frr_reload(cls, value: Path) -> Path:
+        """Ensure FRR Reload script is executable."""
+
+        if isinstance(value, Path):
+            value = Path(value)
+
+        if not os.access(value, os.X_OK):
+            raise ValueError(f"User does not have executable permission to '{value}'")
+
+        return value
 
     class Config:
         """Pydantic Config."""
